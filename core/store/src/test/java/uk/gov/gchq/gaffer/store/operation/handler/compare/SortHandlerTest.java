c0840f5dd377e63fc95bdfe19c295da349750714
John Bergqvist <31512273+JohnLBergqvist@users.noreply.github.com>
2025-10-16 11:22:40 +0100

    Disable spotless ratcheting

--- core/store/src/test/java/uk/gov/gchq/gaffer/store/operation/handler/compare/SortHandlerTest.java
+++ core/store/src/test/java/uk/gov/gchq/gaffer/store/operation/handler/compare/SortHandlerTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.gov.gchq.gaffer.store.operation.handler.compare;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
